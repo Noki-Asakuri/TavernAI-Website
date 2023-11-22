75681638478d85ac9775346df99af341b5113c56
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-11-22 13:22:35 +0700

    refactor(api): Replace 'tiktoken' with 'js-tiktoken' and update usage in tokenRouter

--- src/server/api/routers/tokens.ts
+++ src/server/api/routers/tokens.ts
@@ -1,14 +1,12 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
-import { encoding_for_model } from "tiktoken";
+import { encodingForModel } from "js-tiktoken";
 import z from "zod";
 
 export const tokenRouter = createTRPCRouter({
 	getToken: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
-		const encoding = encoding_for_model("gpt-4-1106-preview");
-
+		const encoding = encodingForModel("gpt-4-1106-preview");
 		const tokens = encoding.encode(input.text);
-		encoding.free();
 
 		return tokens.length;
 	}),
