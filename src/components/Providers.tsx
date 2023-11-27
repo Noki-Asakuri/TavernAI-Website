9ac3aa7973f2d63df8419413500c9a21cbdf327c
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-11-27 14:53:35 +0700

    refactor(components): Remove unused useRouter and simplify Providers component structure

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,18 +3,17 @@
 import { TRPCReactProvider } from "~/trpc/react";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { useRouter } from "next/navigation";
 
 import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children, cookies }: { children: React.ReactNode; cookies: string }) {
-	const router = useRouter();
-
 	return (
-		<NextUIProvider className="flex min-h-screen flex-col" navigate={(path) => router.push(path)}>
-			<NextThemesProvider attribute="class" defaultTheme="dark">
-				<TRPCReactProvider cookies={cookies}>{children}</TRPCReactProvider>
-			</NextThemesProvider>
-		</NextUIProvider>
+		<TRPCReactProvider cookies={cookies}>
+			<NextUIProvider className="flex min-h-screen flex-col">
+				<NextThemesProvider attribute="class" defaultTheme="dark">
+					{children}
+				</NextThemesProvider>
+			</NextUIProvider>
+		</TRPCReactProvider>
 	);
 }
