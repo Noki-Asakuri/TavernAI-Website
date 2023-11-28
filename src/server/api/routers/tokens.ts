2d8ee64943d4961c04054285ac20ab0572b9c529
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-11-28 22:08:58 +0700

    fix(server/api): Return 0 for empty 'text' input in getToken procedure

--- src/server/api/routers/tokens.ts
+++ src/server/api/routers/tokens.ts
@@ -5,6 +5,8 @@ import z from "zod";
 
 export const tokenRouter = createTRPCRouter({
 	getToken: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+		if (!input.text) return 0;
+
 		const encoding = encodingForModel("gpt-4-1106-preview");
 		const tokens = encoding.encode(input.text);
 
