91ffb8ba8b324be0d3a23dc9b0094d75d0932e02
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-11-29 01:23:54 +0700

    feat(Providers): Add useRouter hook to handle navigation in NextUIProvider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,13 +3,16 @@
 import { TRPCReactProvider } from "~/trpc/react";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/navigation";
 
 import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children, cookies }: { children: React.ReactNode; cookies: string }) {
+	const router = useRouter();
+
 	return (
 		<TRPCReactProvider cookies={cookies}>
-			<NextUIProvider className="flex min-h-screen flex-col">
+			<NextUIProvider className="flex min-h-screen flex-col" navigate={(path) => router.push(path)}>
 				<NextThemesProvider attribute="class" defaultTheme="dark">
 					{children}
 				</NextThemesProvider>
