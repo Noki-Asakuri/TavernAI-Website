c2b4ad56e34874cd6b9c4152a6a27ef00b858f7c
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-12-11 20:35:00 +0700

    refactor(components): Extract CategoryChip component for category display in Categories component

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -4,28 +4,38 @@ import Link from "next/link";
 
 import { Chip } from "@nextui-org/react";
 
+const CategoryChip = (props: { name: string; name_view: string; count?: number; href: string }) => {
+	return (
+		<Chip
+			size="md"
+			as={Link}
+			color="success"
+			variant="shadow"
+			className="capitalize"
+			key={["category", props.name].join()}
+			href={props.href}
+		>
+			{props.name_view} {(props.count ?? 0) > 0 && <>({props.count})</>}
+		</Chip>
+	);
+};
+
 export const Categories = async () => {
 	const data = await api.tavern.getCategories.query({ limit: 10 });
 
 	return (
 		<div className="flex flex-wrap gap-2">
-			<Chip size="lg" color="success" variant="shadow" as={Link} href="/characters/categories">
-				Categories
-			</Chip>
+			<CategoryChip href="/characters/categories" name="Categories" name_view="Categories" />
 
 			{data.data?.map((category) => {
 				return (
-					<Chip
-						size="lg"
-						as={Link}
-						color="success"
-						variant="shadow"
-						className="capitalize"
+					<CategoryChip
+						name={category.name}
+						count={category.count}
+						name_view={category.name_view}
 						key={["category", category.name].join()}
 						href={"/characters/categories/" + category.name}
-					>
-						{category.name_view} {(category.count ?? 0) > 0 && <>({category.count})</>}
-					</Chip>
+					/>
 				);
 			})}
 		</div>
