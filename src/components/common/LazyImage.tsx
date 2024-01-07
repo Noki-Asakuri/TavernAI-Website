512926032d0dcd8af7bc759ff908191456d2f9bd
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2024-01-07 11:01:24 +0000

    Refactor LazyImage component to use react-intersection-observer

--- src/components/common/LazyImage.tsx
+++ src/components/common/LazyImage.tsx
@@ -2,50 +2,11 @@
 
 import { Image, type ImageProps } from "@nextui-org/react";
 
-import React, { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 export function LazyImage({ src, alt, ...props }: ImageProps) {
-	// State for storing whether the image is loaded
-	const [isLoaded, setIsLoaded] = useState(false);
+	const { ref, inView } = useInView({ threshold: 0.05, triggerOnce: true });
 
-	// Ref for the image element
-	const imgRef = useRef<HTMLImageElement>(null);
-
-	useEffect(() => {
-		const img = imgRef.current;
-
-		// Ensure IntersectionObserver is available in the browser
-		if (!window.IntersectionObserver) {
-			console.error("IntersectionObserver is not supported by your browser.");
-			setIsLoaded(true); // Fallback to loading images immediately
-			return;
-		}
-
-		// Create a new Intersection Observer instance
-		const observer = new IntersectionObserver(
-			(entries) => {
-				// Find the observed entry
-				const [entry] = entries;
-
-				// If the entry is intersecting, we load the image and unobserve it
-				if (entry?.isIntersecting) {
-					setIsLoaded(true);
-					observer.unobserve(entry.target);
-				}
-			},
-			{ threshold: 0.05 }, // Configure to trigger when at least 1% is visible
-		);
-
-		// Observe the image reference
-		if (imgRef.current) {
-			observer.observe(imgRef.current);
-		}
-
-		// Clean up the observer on unmount
-		return () => {
-			if (img) observer.unobserve(img);
-		};
-	}, []); // Empty dependency array means this effect runs only on mount and unmount
-
-	return <Image src={isLoaded ? src : undefined} alt={alt} ref={imgRef} {...props} />;
+	return <Image src={inView ? src : undefined} alt={alt} ref={ref} {...props} />;
 }
+
