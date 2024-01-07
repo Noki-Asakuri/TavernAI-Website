c370aa9b09c8b5ca36b4969837142d89a6f312a8
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2024-01-07 11:01:36 +0000

    Refactor FavorAuthor and FavorCharacter components

--- src/components/user/FavorAuthor.tsx
+++ src/components/user/FavorAuthor.tsx
@@ -6,12 +6,12 @@ import { FavorButton } from "../common/FavorButton";
 
 import NextLink from "next/link";
 
-import { Avatar, Divider, Link } from "@nextui-org/react";
+import { Avatar, Divider, Link, cn } from "@nextui-org/react";
 
 import { useStore } from "zustand";
 
 const FavorAuthor = () => {
-	const state = useStore(useUserStore, (state) => state);
+	const state = useStore(useUserStore, (state) => ({ favoriteAuthors: state.favoriteAuthors }));
 
 	return (
 		<>
@@ -19,41 +19,54 @@ const FavorAuthor = () => {
 
 			<Divider orientation="horizontal" />
 
-			<div className="grid grid-cols-[repeat(auto-fill,minmax(450px,1fr))] gap-x-2 gap-y-4">
-				{[...state.favoriteAuthors].reverse().map(([name, { name_view }]) => {
-					return (
-						<section key={name} className="flex items-center gap-4">
-							<Avatar
-								showFallback
-								className="h-16 w-16 text-large"
-								src={`https://tavernai.net/users/${name}/img/avatar.webp`}
-							/>
-							<div className="flex flex-col gap-1">
-								<div className="flex items-center justify-start gap-2 text-xl font-bold">
-									<Link as={NextLink} href={"/characters/" + name} underline="hover">
-										<span className="text-xl font-bold">{name_view}</span>
-									</Link>
-									<FavorButton type="author" name={name} name_view={name_view} />
-								</div>
-								<span className="hidden text-sm sm:block sm:space-x-2">
-									<span className="hidden sm:inline-block">Profile: </span>
-									<Link as={NextLink} underline="hover" href={`https://tavernai.net/${name}`}>
-										https://tavernai.net/{name}
-									</Link>
-								</span>
-
-								<span className="block text-sm sm:hidden">
-									<Link as={NextLink} underline="hover" href={`https://tavernai.net/${name}`}>
-										Profile
-									</Link>
-								</span>
-							</div>
-						</section>
-					);
+			<div
+				className={cn("grid grid-cols-1", {
+					"grid-cols-[repeat(auto-fill,minmax(450px,1fr))] gap-x-2 gap-y-4": state.favoriteAuthors.size > 0,
 				})}
+			>
+				{state.favoriteAuthors.size === 0 && (
+					<div>
+						<p className="text-center">{"You don't have any favorite authors yet."}</p>
+					</div>
+				)}
+
+				{state.favoriteAuthors.size > 0 &&
+					[...state.favoriteAuthors].reverse().map(([name, { name_view }]) => {
+						return (
+							<section key={name} className="flex items-center gap-4">
+								<Avatar
+									showFallback
+									className="h-16 w-16 text-large"
+									src={`https://tavernai.net/users/${name}/img/avatar.webp`}
+								/>
+
+								<div className="flex flex-col gap-1">
+									<div className="flex items-center justify-start gap-2 text-xl font-bold">
+										<Link as={NextLink} href={"/characters/" + name} underline="hover">
+											<span className="text-xl font-bold">{name_view}</span>
+										</Link>
+										<FavorButton type="author" name={name} name_view={name_view} />
+									</div>
+									<span className="hidden text-sm sm:block sm:space-x-2">
+										<span className="hidden sm:inline-block">Profile: </span>
+										<Link as={NextLink} underline="hover" href={`https://tavernai.net/${name}`}>
+											https://tavernai.net/{name}
+										</Link>
+									</span>
+
+									<span className="block text-sm sm:hidden">
+										<Link as={NextLink} underline="hover" href={`https://tavernai.net/${name}`}>
+											Profile
+										</Link>
+									</span>
+								</div>
+							</section>
+						);
+					})}
 			</div>
 		</>
 	);
 };
 
 export default FavorAuthor;
+
--- src/components/user/FavorCharacter.tsx
+++ src/components/user/FavorCharacter.tsx
@@ -4,12 +4,12 @@ import { useUserStore } from "~/server/store";
 
 import { CharacterCard } from "../characters/CharacterCard";
 
-import { Divider } from "@nextui-org/react";
+import { Divider, cn } from "@nextui-org/react";
 
 import { useStore } from "zustand";
 
 const FavorCharacter = () => {
-	const state = useStore(useUserStore, (state) => state);
+	const state = useStore(useUserStore, (state) => ({ favoriteCharacters: state.favoriteCharacters }));
 
 	return (
 		<>
@@ -17,35 +17,48 @@ const FavorCharacter = () => {
 
 			<Divider orientation="horizontal" />
 
-			<div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] place-items-center gap-x-2 gap-y-4">
-				{[...state.favoriteCharacters]
-					.reverse()
-					.map(([public_id, { author, description, name, public_id_short }]) => {
-						return (
-							<CharacterCard
-								key={public_id}
-								character={{
-									create_date: "",
-									edit_date: "",
-									id: 0,
-									moderation: 0,
-									name,
-									nsfw: 0,
-									private: 0,
-									public_id,
-									public_id_short,
-									short_description: description,
-									status: 0,
-									user_id: "",
-									user_name: author,
-									user_name_view: author,
-								}}
-							/>
-						);
-					})}
+			<div
+				className={cn("grid grid-cols-1", {
+					"grid-cols-[repeat(auto-fill,minmax(240px,1fr))] place-items-center gap-x-2 gap-y-4":
+						state.favoriteCharacters.size > 0,
+				})}
+			>
+				{state.favoriteCharacters.size === 0 && (
+					<div>
+						<p className="text-center">{"You don't have any favorite characters yet."}</p>
+					</div>
+				)}
+
+				{state.favoriteCharacters.size > 0 &&
+					[...state.favoriteCharacters]
+						.reverse()
+						.map(([public_id, { author, description, name, public_id_short }]) => {
+							return (
+								<CharacterCard
+									key={public_id}
+									character={{
+										create_date: "",
+										edit_date: "",
+										id: 0,
+										moderation: 0,
+										name,
+										nsfw: 0,
+										private: 0,
+										public_id,
+										public_id_short,
+										short_description: description,
+										status: 0,
+										user_id: "",
+										user_name: author,
+										user_name_view: author,
+									}}
+								/>
+							);
+						})}
 			</div>
 		</>
 	);
 };
 
 export default FavorCharacter;
+
